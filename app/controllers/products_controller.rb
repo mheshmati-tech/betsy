5b6b9aa3d7768c7f31c7a31490bb6c7df7ad4e1a
Maryam Heshmati <mairheshmati@gmail.com>
2020-06-10 10:54:57 -0700

    added routes for products, added index, edit, show to the products controller, added partial forms for edit and show.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,36 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_product
+    @product = Product.find_by(id: params[:id])
+    if @product.nil?
+      redirect_to root_path #TODO - what is the best thing to do when prodcut is not found
+      return
+    end
+  end
 end
