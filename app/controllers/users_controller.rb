069ad27d0366312c3b8153d528b50239f426beac
Katie <katie.vandervoort@gmail.com>
2020-06-10 10:53:34 -0700

    created the index view for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
+USERS = [{username: 'hi',email: 'abc@gmail.com', uid: 12345, provider: 'github'}]
+
+
 class UsersController < ApplicationController
+
+  def index
+    @users = USERS
+  end
+
+  def show
+    # @user = User.find_by(id: params[:id])
+    # render_404 unless @user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # OAuth routes
+  get "/auth/github", as: "github_login"
+  get "/auth/:provider/callback", to: "users#create"
+  delete "/logout", to: "users#destroy", as: "logout"
+
+  resources :users, only: [:index, :show]
+
 end
