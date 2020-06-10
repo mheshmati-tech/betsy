3fb5f1ee63537e135fd25ea757042b26c794eb4a
Maryam Heshmati <mairheshmati@gmail.com>
2020-06-10 11:37:40 -0700

    added validations for the products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  #TODO product belongs to a user
+  # belongs_to :user
+
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
