72594fd7898b8dee4821ab788876974c3be6fffd
Maryam Heshmati <mairheshmati@gmail.com>
2020-06-11 13:46:21 -0700

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_172204) do
+ActiveRecord::Schema.define(version: 2020_06_10_212846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,14 @@ ActiveRecord::Schema.define(version: 2020_06_10_172204) do
     t.string "photo_url"
     t.integer "stock"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "uid"
+    t.string "provider"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
