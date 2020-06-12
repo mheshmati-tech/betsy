2d8ca2a415841a98afb506e9ad2d9d65fc79e623
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-06-12 14:52:47 -0700

    resolved conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_173625) do
+ActiveRecord::Schema.define(version: 2020_06_12_174158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "order_items", force: :cascade do |t|
     t.integer "quantity"
@@ -48,6 +54,13 @@ ActiveRecord::Schema.define(version: 2020_06_12_173625) do
     t.integer "stock"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "products_categories_joins", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_products_categories_joins_on_category_id"
+    t.index ["product_id"], name: "index_products_categories_joins_on_product_id"
   end
 
   create_table "users", force: :cascade do |t|
