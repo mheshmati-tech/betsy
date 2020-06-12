51247dbf52c87b988cfa2969f4e46e729fc2cfff
Maryam Heshmati <mairheshmati@gmail.com>
2020-06-12 12:13:53 -0700

    we added model method to calculate the total price for the cart and added that to the show

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
   has_many :order_items
 
+  def calculate_order_total
+    order_items = OrderItem.where(order_id: self.id) # array of order items
+    return order_items.map { |order_item| order_item.calculate_total }.sum
+  end
+
 end
