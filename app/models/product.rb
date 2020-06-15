8d52df5e623f04d601a1bf6a47c253666650c337
luckySeattleMonkey <freeMonkey19@gmail.com>
2020-06-15 13:52:43 -0700

    fix conflict

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,8 @@
 class Product < ApplicationRecord
   #TODO product belongs to a user
   belongs_to :user
-<<<<<<< HEAD
-  has_and_belongs_to_many  :categories
-
-=======
   has_many :order_items
   has_and_belongs_to_many :categories
->>>>>>> 0766c9b716f919946dab82dee24ca009e14c16be
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0 }
