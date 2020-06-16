38330d1b61ed10eb3d273b9ab71b82cd2a9df3f8
Maryam Heshmati <mairheshmati@gmail.com>
2020-06-15 18:03:14 -0700

    added model validations for Order and Order items, changed the schema so that Order item has an order status

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,8 @@ class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+
   def calculate_total
     product = Product.find_by(id: self.product_id)
     return self.quantity * product.price
