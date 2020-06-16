b430ec7df87c511a614aa50ea8b00f7420a325bc
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-06-16 10:55:48 -0700

    Added Review average_rating method and added average rating to product show view

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,5 +8,15 @@ class Product < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0 }
+
+  def average_rating 
+    length = self.reviews.length
+
+    return 0 if length == 0 
+      
+    total_rating = self.reviews.sum { |review| review.rating }
+    
+    return (total_rating / length).round(1)
+  end
   
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :product
 
   validates :rating, presence: true
+
 end
