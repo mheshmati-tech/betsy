aa98ffabf1fbbf63b6fc943988957c8e7e0a6a74
Maryam Heshmati <mairheshmati@gmail.com>
2020-06-17 12:43:29 -0700

    added functionality to render the amount of stock order items as a drop down menue when selecting an item, and implemented a default picture for a product that does not have a pciture

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,6 +16,11 @@ class ProductsController < ApplicationController
     @product = Product.new(
       product_params
     )
+
+    if @product.photo_url.empty?
+      @product.photo_url = "https://lh3.googleusercontent.com/0hJSje8_Z034MV_zDnQFGVt1Id-P21tDb3AP-J7WFtH0ITgL47YvUV642kZg1fXxtZaO47A14pRjWP2tG-2AlHm7jst2i8mB-yZQPX0I67SB4X9iJs4Co9lge5RlgsMoK8BeP1pXNtKuQkneNuBrxPr-x6n2KpVj7tA_E4k1krdZPj6Z_kZT8XzNIN_ztY3rD9HdwInpgaQ7HYImlTdGo_A50RoLEJ-7sK2_gFJAbSLIwnxEAAcaw7ELKxhO0p2QUbBk8Qwp3GnleViM2LVIUIQMcQDt7JvcKgPWH1DpNYpv8GRitVPOIBOt4mVvsVSJ5I6iBXU_7Tz1b1HQC97OajROBAWNSYryi_wqrac6MjfwFS0VbRxiAthPtUxApWpwTmxSF8LTqtUh2MQfLnOGp4wnX-CkfIQzqazG20xxXvsv24iLE1PZwjwkFb2vE8fglhUr0PAvEHseJz7D1PJwt5MtH38gdDPTK8o8WhJ1R4vGaiCqcekvR6OUGy4DYUEjW-zd3psZpwuQLxPLlgefvSvL4lErgVrxV8bg61cn7WcQ2Avev4L36DAS40RyY0cqInt_CJ-9Ovp3oRPTwTp0EwEL2XmHHKoW8yaSgJloyjAzWT9SHAAnoAoTjHWeHvXot8qdWvT8dwmq-LRsR_-cpE5-qNo2ejrkDUrCD5k7R35r5F_TKXLG9CX-Tkrf7g=w600-h512-no?authuser=0"
+    end
+
     if !@logged_user
       flash[:error] = "You must be logged in to create a new product."
       redirect_to root_path
--- app/helpers/order_items_helper.rb
+++ app/helpers/order_items_helper.rb
@@ -1,2 +1,5 @@
 module OrderItemsHelper
+  def make_range(number)
+    (1..number).to_a
+  end
 end
