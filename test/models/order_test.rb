9f92b1f136a73813ed5dc732596640b4024d3d46
Katie <katie.vandervoort@gmail.com>
2020-06-17 20:09:51 -0700

    Setting up order testing

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,12 +1,12 @@
 require "test_helper"
 
 describe Order do
-  # describe 'validations' do
-  #   it 'is valid when all fields are present' do
-  #     order = orders(:order1)
-  #     result = order.valid?
+  describe 'validations' do
+    it 'is valid when all fields are present' do
+      order = orders(:order1)
+      result = order.valid?
 
-  #     expect(result).must_equal true
-  #   end
-  # end
+      expect(result).must_equal true
+    end
+  end
 end
