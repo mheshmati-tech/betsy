78284c424246199892597e150003a62f09e2b140
luckySeattleMonkey <freeMonkey19@gmail.com>
2020-06-18 20:34:46 -0700

    more testing

--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -1,9 +1,17 @@
 require "test_helper"
 
 describe HomepagesController do
+ 
   it "must get index" do
-    get homepages_index_url
+    get products_path
+    must_respond_with :success
+  end
+
+  it "must get meet the team path" do
+    get meet_the_team_path
     must_respond_with :success
   end
 
 end
+
+
